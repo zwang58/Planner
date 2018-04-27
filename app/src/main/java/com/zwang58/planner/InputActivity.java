f2d52c95f75dbc80e13101871966169dfead0da1
Jason <zwang58@ucsc.edu>
2018-04-27 11:53:52 -0700

    454

--- app/src/main/java/com/zwang58/planner/InputActivity.java
+++ app/src/main/java/com/zwang58/planner/InputActivity.java
@@ -1,10 +1,12 @@
 package com.zwang58.planner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,6 +43,11 @@ public class InputActivity extends AppCompatActivity {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+
+                String toast = "title: " + t + "\ndesc: " + d + "\ntime: " + "\ngps: ";
+                Toast.makeText(InputActivity.this, toast, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(this, MainActivity);
 
 
 
