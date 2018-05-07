f7d5f807fc221f716afa9a6fea247b255a3d00c1
Jason <32943110+zwang58@users.noreply.github.com>
2018-05-07 14:44:27 -0700

    5454

--- app/src/main/java/com/zwang58/planner/InputActivity.java
+++ app/src/main/java/com/zwang58/planner/InputActivity.java
@@ -1,16 +1,30 @@
 package com.zwang58.planner;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -18,6 +32,9 @@ public class InputActivity extends AppCompatActivity {
 
     private EditText title, desc;
     private Button create;
+    public JSONObject jo = null;
+    public JSONArray ja = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,35 +44,85 @@ public class InputActivity extends AppCompatActivity {
         desc = findViewById(R.id.desc_input);
         create = findViewById(R.id.create_button);
 
+        try{
+            File f = new File(getFilesDir(), "file.ser");
+            FileInputStream fi = new FileInputStream(f);
+            ObjectInputStream o = new ObjectInputStream(fi);
+            // Notice here that we are de-serializing a String object (instead of
+            // a JSONObject object) and passing the String to the JSONObject’s
+            // constructor. That’s because String is serializable and
+            // JSONObject is not. To convert a JSONObject back to a String, simply
+            // call the JSONObject’s toString method.
+            String j = null;
+            try{
+                j = (String) o.readObject();
+            }
+            catch(ClassNotFoundException c){
+                c.printStackTrace();
+            }
+            try {
+                jo = new JSONObject(j);
+                ja = jo.getJSONArray("data");
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+        }
+        catch(IOException e){
+            // Here, initialize a new JSONObject
+            jo = new JSONObject();
+            ja = new JSONArray();
+            try{
+                jo.put("data", ja);
+            }
+            catch(JSONException j){
+                j.printStackTrace();
+            }
+        }
+
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String t = title.getText().toString();
                 String d = desc.getText().toString();
-                JSONObject item = new JSONObject();
                 Date now = Calendar.getInstance().getTime();
 
+                JSONObject temp = new JSONObject();
                 try {
-                    item.put("title",t);
-                    item.put("desc",d);
-                    item.put("time",now.getTime());
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    temp.put("title", t);
+                    temp.put("desc", d);
+                    temp.put("time",now.toString());
+                }
+                catch(JSONException j){
+                    j.printStackTrace();
                 }
 
-                String toast = "title: " + t + "\ndesc: " + d + "\ntime: " + "\ngps: ";
+                ja.put(temp);
+
+                // write the file
+                try{
+                    File f = new File(getFilesDir(), "file.ser");
+                    FileOutputStream fo = new FileOutputStream(f);
+                    ObjectOutputStream o = new ObjectOutputStream(fo);
+                    String j = jo.toString();
+                    o.writeObject(j);
+                    o.close();
+                    fo.close();
+                }
+                catch(IOException e){
+
+                }
+
+                String toast = "title: " + t + "\ndesc: " + d + "\ntime: " + now.toString() + "\ngps: ";
                 Toast.makeText(InputActivity.this, toast, Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(this, MainActivity);
-
-
-
+                Intent intent = new Intent(InputActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
             }
         });
 
-
-
-
     }
+
+
 }
--- app/src/main/java/com/zwang58/planner/MainActivity.java
+++ app/src/main/java/com/zwang58/planner/MainActivity.java
@@ -1,16 +1,134 @@
 package com.zwang58.planner;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    public JSONObject jos = null;
+    public JSONArray ja = null;
     private ListView list;
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = findViewById(R.id.mainList);
 
+    }
+
+    protected void onResume(){
+        super.onResume();
+        TextView text = findViewById(R.id.text);
+        text.setVisibility(View.INVISIBLE);
+
+        Log.d(TAG, ""+getFilesDir());
+
+        jos = null;
+        try{
+            // Reading a file that already exists
+            File f = new File(getFilesDir(), "file.ser");
+            FileInputStream fi = new FileInputStream(f);
+            ObjectInputStream o = new ObjectInputStream(fi);
+            // Notice here that we are de-serializing a String object (instead of
+            // a JSONObject object) and passing the String to the JSONObject’s
+            // constructor. That’s because String is serializable and
+            // JSONObject is not. To convert a JSONObject back to a String, simply
+            // call the JSONObject’s toString method.
+            String j = null;
+            try{
+                j = (String) o.readObject();
+            }
+            catch(ClassNotFoundException c){
+                c.printStackTrace();
+            }
+            try {
+                jos = new JSONObject(j);
+                ja = jos.getJSONArray("data");
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+
+            // Show the list
+            final ArrayList<ListData> aList = new ArrayList<ListData>();
+            for(int i = 0; i < ja.length(); i++){
+
+                ListData ld = new ListData();
+                try {
+                    ld.firstText = ja.getJSONObject(i).getString("first");
+                    ld.secondText = ja.getJSONObject(i).getString("second");
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
+                }
+
+                aList.add(ld);
+            }
+
+            // Create an array and assign each element to be the title
+            // field of each of the ListData objects (from the array list)
+            String[] listItems = new String[aList.size()];
+
+            for(int i = 0; i < aList.size(); i++){
+                ListData listD = aList.get(i);
+                listItems[i] = listD.firstText;
+            }
+
+            // Show the list view with the each list item an element from listItems
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+            list.setAdapter(adapter);
+
+            // Set an OnItemClickListener for each of the list items
+            final Context context = this;
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    ListData selected = aList.get(position);
+
+                    // Create an Intent to reference our new activity, then call startActivity
+                    // to transition into the new Activity.
+                    Intent detailIntent = new Intent(context, DetailActivity.class);
+
+                    // pass some key value pairs to the next Activity (via the Intent)
+                    detailIntent.putExtra("first", selected.firstText);
+                    detailIntent.putExtra("second", selected.secondText);
+
+                    startActivity(detailIntent);
+                }
+
+            });
+        }
+        catch(IOException e){
+            // There's no JSON file that exists, so don't
+            // show the list. But also don't worry about creating
+            // the file just yet, that takes place in AddText.
+
+            //Here, disable the list view
+            list.setEnabled(false);
+            list.setVisibility(View.INVISIBLE);
+
+            //show the text view
+            text.setVisibility(View.VISIBLE);
+        }
     }
 }
